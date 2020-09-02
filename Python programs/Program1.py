d7568b5482ebb56e2adad00459169cf8f54a69db
Rohan Mahaveer <38761761+rohanmahaveer@users.noreply.github.com>
2020-09-02 10:03:05 +0530

    Add files via upload

---
+++ Python programs/Program1.py
@@ -0,0 +1,9 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+'''
+firstname = str(input("Input your First Name : "))
+lastname = str(input("Input your Last Name : "))
+print ("" + lastname + " " + firstname)
